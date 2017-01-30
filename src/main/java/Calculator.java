ef09e080815d0a1e2224423bb1bec26d3254a025
Adrian Yadir Jimenez Carrillo <ajimenezc@pslcol.com.co>
2017-01-30 17:03:32 -0500

    Solved still item 4

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -5,6 +5,25 @@ public class Calculator {
 		if (numbers.equals("")) {
 			return 0;	
 		} else {
+			String comma = ",";
+			String newLines = "\n";
+			String delimiters = new String();
+			int beginFor = 0;
+			if(numbers.charAt(0) == '/' && numbers.charAt(1)=='/') {
+				delimiters = "["+newLines+"\\"+numbers.substring(2, 3)+"\\"+comma+"]";
+				beginFor = 2;
+			}else{
+				delimiters = "["+newLines+"\\"+comma+"]";
+			}
+			if(numbers.contains(comma) || numbers.contains(newLines)){
+				
+				String[] params = numbers.split((delimiters));
+				int total = 0;
+				for (int i = beginFor; i < params.length; i++) {
+					total += Integer.parseInt(params[i]);					
+				}
+				return total;
+			}
 			return Integer.parseInt(numbers);
 		}
 		
--- src/test/java/TestCalculator.java
+++ src/test/java/TestCalculator.java
@@ -16,4 +16,32 @@ public class TestCalculator {
 		int result = calculator.Add("5");
 		Assert.assertEquals(5, result);
 	}
+	
+	@Test
+	public void twoStringShouldReturnAddResult(){
+		Calculator calculator = new Calculator();
+		int result = calculator.Add("2,5");
+		Assert.assertEquals(7, result);
+	}
+	
+	@Test
+	public void fourStringShouldReturnAddResult(){
+		Calculator calculator = new Calculator();
+		int result = calculator.Add("3,2,5,8");
+		Assert.assertEquals(18, result);
+	}
+	
+	@Test
+	public void twoWaysSeparateString(){
+		Calculator calculator = new Calculator();
+		int result = calculator.Add("4\n2\n4,8");
+		Assert.assertEquals(18, result);
+	}
+	
+	@Test
+	public void anyDelimiter(){
+		Calculator calculator = new Calculator();
+		int result = calculator.Add("//*\n1*2");
+		Assert.assertEquals(3, result);
+	}
 }
